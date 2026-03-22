81b8e6a23c40ab1861849e9419d42e7456ec9b92
Kacper Sobieraj <255692@edu.p.lodz.pl>
2026-03-22 11:25:49 +0100

    9.1 w pliku DefaultCountingOutRhymer zmieniono widoczność niektórych zmiennych i metod (STACK_EMPTY, STACK_ELEMENTS, STACK_EMPTY, callCheck, isFull)

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -3,22 +3,22 @@ package edu.kis.vh.nursery;
 public class DefaultCountingOutRhymer {
 
     public static final int SIZE = 12;
-    public static final int STACK_EMPTY = -1;
-    public static final int STACK_ELEMENTS = SIZE - 1;
+    protected static final int STACK_EMPTY = -1;
+    private static final int STACK_ELEMENTS = SIZE - 1;
     private final int[] numbers = new int[SIZE];
 
-    public int total = STACK_EMPTY;
+    private int total = STACK_EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
-    public boolean callCheck() {
+    protected boolean callCheck() {
         return total == STACK_EMPTY;
     }
 
-    public boolean isFull() {
+    protected boolean isFull() {
         return total == STACK_ELEMENTS;
     }
 
